59d37792618f4e1b32a410d41171e909a43b1f58
NICUP14 <62835945+NICUP14@users.noreply.github.com>
2021-03-09 17:53:09 +0200

    Add files via upload

--- xml.h
+++ xml.h
@@ -1,31 +1,48 @@
-#include <iostream>
-
-unsigned long strLen(char* s)
+unsigned int length(char *s)
 {
-	///Returns the number of characters before null
+	///Returns number of characters before null
 	char* i=s;
 	while(*i)
 		i++;
-	return i-s;
+	return s-i;
 }
 
-char* strChr(char* s, char* s2, char c, bool m)
+char* find(char* s, char* s2, char c)
 {
-	///Searches address containing c between memory ranges s-s2
+	///Returns first occurence of character c between adress ranges s and s2
 	if(s==0 || s2==0)
 		return 0;
-	int j=s<s2?1:-1;
-	while(j>0?s<=s2:s>=s2)
+	while(*s!=0 && s<=s2)
 	{
-		if(*s && (*s==c)==m)
+		if(*s==c)
 			return s;
-		s+=j;
+		s++;
 	}
 	return 0;
 }
 
-void strChr2(char* s, char* s2, bool f[256])
+void word(char* s, char* s2, char* &s3, char* &s4)
 {
+	///Extracts first word between adress ranges s, s2 via iterators
+	if(s==0 || s2==0)
+		return;
+	while(*s!=0 && s<=s2 && *s==' ')
+		s++;
+	if(*s==0 || s>s2)
+	{
+		s3=s4=0;
+		return;
+	}
+	else
+		s3=s;
+	while(*s!=0 && s<=s2 && *s!=' ')
+		s++;
+	s4=(s>s2)?s2:s-1;
+}
+
+void mark(char* s, char* s2, bool *f)
+{
+	///Marks present characters between adress ranges s, s2
 	for(int aux=0;aux<256;aux++)
 		f[aux]=0;
 	if(s==0 || s2==0)
@@ -37,122 +54,163 @@ void strChr2(char* s, char* s2, bool f[256])
 	}
 }
 
-void strPrint(char* s, char* s2)
+void print(char* s, char* s2)
 {
+	///Prints all characters between adress ranges s, s2 also making spaces visible (debug)
+	if(s==0 || s2==0)
+		return;
 	while(*s && s<=s2)
 	{
-		if(*s!=' ')
-			std::cout << *s;
+		if(*s==' ')
+			std::cout << "[space]";
 		else
-			std::cout << "X";
+			std::cout << *s;
 		s++;
 	}
 	std::cout << '\n';
 }
 
-struct Node
+void xmlParser(char* data)
 {
-	char *name, *value;
-	Node *next=0, *previous=0, *parent=0, *children=0;
-};
-
-Node* xmlParser(char* content)
-{
-	char *i, *i2, *i3, *i4, *i5, *i6, *i7, *i8, *i9;
-	unsigned int n=strLen(content),f2[n]={0};
-	bool f[256];
-	Node aux, *tree=0, *pathBegin=0, *pathEnd=0;
-
-	i3=content;
-	while(i3)
+	bool lvalue, equal, f[256];
+	unsigned int n=length(data);
+	char *i, *i2, *i3, *i4, *i5, *i6, *i7, *i8, *i9, *i10;
+	
+	i3=data;
+	if(*i3!=0 && *i3!='<')
 	{
-		i=i3;
-		i2=strChr(i, content+n, '>', true);
-		i3=strChr(i2, content+n, '<', true);
-		if(i3 && i3<i2)
-		{
-			std::cout << "\"Less than\" character detected inside tag";
-			return 0;
-		}
-
-		i++;
-		i2--;
-
-		///Element name extraction
-		i4=strChr(i, i2, ' ', true);
-		if(i4==0)
-			i4=i2;
-		else
-			i4--;
-		std::cout << "Name: ";
-		strPrint(i,i4);
-
-		std::cout << "Debug: " << i << ' ' << i4 << '\n';
-		if(*i==' ' || *i4=='<')
-		{
-			std::cout << "Name is empty\n";
-			return 0;
-		}
-		
-		strChr2(i, i4, f);
-		if(f[34]==1 || f[39]==1 || f['='])
-		{
-			std::cout << "Name contains invalid characters\n";
-			return 0;
-		}
-
-		///Attribute extraction
-		if(i4!=i2)
-		{	
-			i5=strChr(i4+1, i2, ' ' , false);
-			if(i5 && *i5=='=')
-			{
-				std::cout << "Left extremity attribute lvalue missing\n";
-				return 0;
-			}
-			i5=strChr(i2, i4+1, ' ', false);
-			if(i5 && *i5=='=')
-			{
-				std::cout << "Right extremity attribute rvalue missing\n";
-				return 0;
-			}
-			
-			i5=strChr(i4+1, i2, '=', true);
-			while(i5)
-			{
-				///Attribute lvalue
-				i6=strChr(i5-1, i4+1, ' ', false);
-				i7=strChr(i6, i4+1, ' ', true)+1;
-				strChr2(i7, i6, f);
-				if(f['_']==1 || f[34]==1 || f[39]==1)
-				{
-					std::cout << "Attribute lvalue contains invalid characters\n";
-					return 0;
-				}
-				std::cout << "Attribute lvalue: ";
-				strPrint(i7, i6);
-
-				///Attribute rvalue
-				i8=strChr(i5+1, i2, ' ', false);
-				if(*i8!=34 && *i8!=39)
-				{
-					std::cout << "Attribute rvalue not quoted\n";
-					return 0;
-				}
-				i9=strChr(i8+1, i2, *i8, true);
-				if(i9==0)
-				{
-					std::cout << "Attribute lvalue closing quotes missing\n";
-					return 0;
-				}
-				std::cout << "Attribute rvalue: ";
-				strPrint(i8, i9);
-
-				i5=strChr(i5+1, i2, '=', true);
-			}
-		}
-
+		std::cout << "Invalid characters before xml data\n";
+		return;
 	}
 
-}
+	while(i3)
+	{
+		///Tag and text splitting
+		i=i3;
+		i2=find(i+1, data+n, '>');
+		i3=find(i2+1, data+n, '<');
 
+		///Empty name exception
+		if(i+1==i2 || *(i+1)==' ')
+		{
+			std::cout << "Empty name\n";
+			return;
+		}
+
+		///Tag name extraction and validation
+		word(i+1, i2-1, i4, i5);
+		mark(i4, i5, f);
+		if(f[(int)'<']==1 || f[(int)'=']==1 || f[(int)'_']==1 || f[(int)'\"']==1 || f[(int)'\"']==1)
+		{
+			std::cout << "Invalid characters in name\n";
+			return;
+		}
+
+		std::cout << "Name: ";
+		print(i4, i5);
+
+		///Tag attribute parser
+		if(i4!=0)
+		{
+			lvalue=0;
+			equal=0;
+
+			word(i5+1, i2-1, i4, i5);
+			while(i4!=0)
+			{
+				i9=i10=0;
+				i6=find(i4, i5, '=');
+				
+				///Assignment handler
+				if(equal!=lvalue)
+				{
+					std::cout << "Missing equal in attribute assignment\n";
+					return;
+				}
+				if(i6!=0)
+				{
+					if(equal==1)
+					{
+						std::cout << "Extra equal in attribute assignment\n";
+						return;
+					}
+					equal=1;
+				}
+
+				if(i8>i7)
+				{
+					std::cout << "Missing lvalue\n";
+					return;
+				}
+
+				if(i6==i4 && i6==i5)
+					lvalue=1;
+				else
+				{
+					if(lvalue==0)
+					{
+						///lvalue
+						i7=i4;
+						i8=(i6==0)?i5:i6-1;
+						mark(i7, i8, f);
+						
+
+
+						if(f[(int)'<']==1 || f[(int)'=']==1 || f[(int)'\"']==1 || f[(int)'\'']==1)
+						{
+							std::cout << "invalid characters in lvalue\n";
+							return;
+						}
+						///Rvalue clipped to lvalue
+						if(i6!=0 && i5!=i6)
+							goto rvalue;
+						lvalue=1;
+					}
+					else if(lvalue==1)
+					{
+						///Rvalue
+						rvalue:
+
+						i9=(i6==0)?i4:i6+1;
+						if(*i9!='\"' && *i9!='\'')
+						{
+							std::cout << "Missing rvalue opening quotes\n";
+							return;
+						}
+						i10=find(i9+1, i2-1, *i9);
+						if(i10==0)
+						{
+							std::cout << "Missing rvalue closing quotes\n";
+							return;
+						}
+
+						mark(i9, i10, f);
+						if(f[(int)'<']==1 || (i10<i2-1 && *(i10+1)!=' '))
+						{
+							std::cout << "Invalid characters in rvalue\n";
+							return;
+						}
+
+						i9++;
+						i10--;
+						lvalue=0;
+						equal=0;
+					}
+					if(i9!=0)
+					{
+						std::cout << "Lvalue: ";
+						print(i7, i8);
+						std::cout << "Rvalue: ";
+						print(i9, i10);
+					}
+				}
+				word(i5+1, i2-1, i4, i5);
+			}
+			if(lvalue==1)
+			{
+				std::cout << "Missing rvalue\n";
+				return;
+			}
+		}
+	}
+}
